9d5234a856a5ac4ce327cedde08e5821a1d577d0
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-02-22 19:23:00 -0600

    Added PSA_Sub

--- Phase-1/PSA_16bit_tb.sv
+++ Phase-1/PSA_16bit_tb.sv
@@ -148,8 +148,8 @@ module PSA_16bit_tb();
     expected_sum = '{default: 4'h0}; // initialize the expected sum array
     expected_PSA_sum = 16'h0000; // initialize expected PSA_16bit sum
     expected_PSA_error = 1'b0; // initialize expected error flag
-    pos_overflow = '{default: 4'h0}; // initialize expected pos overflow
-    neg_overflow = '{default: 4'h0}; // initialize expected neg overflow
+    pos_overflow = 4'h0; // initialize expected pos overflow
+    neg_overflow = 4'h0; // initialize expected neg overflow
     addition_operations = 17'h00000; // initialize addition operation count
     error = 1'b0; // initialize error flag
 

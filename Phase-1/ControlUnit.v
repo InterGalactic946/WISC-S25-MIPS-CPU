fbc4b0dd55802bd0cb514cc7ea037d8d23937c9f
ssankar25 <awesomeunni25@gmail.com>
2025-02-28 10:09:31 -0600

    Added PCS Control Signal

--- Phase-1/ControlUnit.v
+++ Phase-1/ControlUnit.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module ControlUnit(Opcode, ALUSrc, MemtoReg, RegWrite, RegSrc1, MemRead, MemWrite, Branch, HLT, ALUOp);
+module ControlUnit(Opcode, ALUSrc, MemtoReg, RegWrite, RegSrc1, MemRead, MemWrite, Branch, HLT, PCS, ALUOp);
 
     input wire [3:0] Opcode; // Opcode of the current instruction
     output wire ALUSrc; // Determines whether to use the immediate or register-value as the ALU input
@@ -11,6 +11,7 @@ module ControlUnit(Opcode, ALUSrc, MemtoReg, RegWrite, RegSrc1, MemRead, MemWrit
     output wire MemWrite; // Looks for whether the memory unit is written to in this operation
     output wire Branch; // Used to signal that the PC should take the value from the branch adder
     output wire HLT; // Used to signal an HLT instruction
+    output wire PCS; // Used to signal a PCS instruction
     output wire [3:0] ALUOp; // Control lines into the ALU to allow for the unit to determine its operation
 
     // ALUSrc must be 1 for SLL, SRA, ROR, LW, SW, LLB, and LHB
@@ -40,6 +41,9 @@ module ControlUnit(Opcode, ALUSrc, MemtoReg, RegWrite, RegSrc1, MemRead, MemWrit
 
     // HLT Opcode = 1111
     assign HLT = &Opcode[3:0];
+
+    // PCS Opcode = 1110
+    assign PCS = Opcode[3] & Opcode[2] & Opcode[1] & ~Opcode[0];
 
 endmodule
 

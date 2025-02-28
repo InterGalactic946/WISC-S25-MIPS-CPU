824133442746d124be0601c5b479077f753f4ce8
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-02-28 16:45:26 -0600

    Finished CPU module.

--- Phase-1/ControlUnit.v
+++ Phase-1/ControlUnit.v
@@ -1,18 +1,21 @@
 `default_nettype none
 
-module ControlUnit(Opcode, ALUSrc, MemtoReg, RegWrite, RegSrc1, MemRead, MemWrite, Branch, HLT, PCS, ALUOp);
+module ControlUnit(Opcode, ALUSrc, MemtoReg, RegWrite, RegSrc, MemEnable, MemWrite, Branch, HLT, PCS, ALUOp, Z_en, NV_en);
 
     input wire [3:0] Opcode; // Opcode of the current instruction
     output wire ALUSrc;      // Determines whether to use the immediate or register-value as the ALU input
     output wire MemtoReg;    // Allows for choosing between writing from the ALU or memory output to the register file
     output wire RegWrite;    // Determines if the register file is being written to
-    output wire RegSrc1;     // Determines if the read register port 1 should use rs or rd, which is read from for LLB/LHB operations
+    output wire RegSrc;      // Determines if the read register port 1 should use rs or rd, which is read from for LLB/LHB operations
     output wire MemEnable;   // Looks for whether the memory unit is used in this operation
     output wire MemWrite;    // Looks for whether the memory unit is written to in this operation
     output wire Branch;      // Used to signal that the PC should take the value from the branch adder
     output wire HLT;         // Used to signal an HLT instruction
     output wire PCS;         // Used to signal a PCS instruction
     output wire [3:0] ALUOp; // Control lines into the ALU to allow for the unit to determine its operation
+    output wire Z_en;        // Signal to turn on the Z flag registers
+    output wire NV_end;      // Signal to turn on the N and V flag registers
+
 
     // ALUSrc must be 1 for SLL, SRA, ROR, LW, SW, LLB, and LHB
     assign ALUSrc = (Opcode[3]) | (Opcode[2] & ~Opcode[1]) | (Opcode[2] & Opcode[1] & ~Opcode[0]);
@@ -23,10 +26,10 @@ module ControlUnit(Opcode, ALUSrc, MemtoReg, RegWrite, RegSrc1, MemRead, MemWrit
     // RegWrite must be 1 for ADD, SUB, XOR, RED, SLL, SRA, ROR, PADDSB, LW, LLB, LHB, and PCS
     assign RegWrite = (~Opcode[3]) | (Opcode[1]) | (Opcode[3] & ~Opcode[2] & ~Opcode[0]);
 
-    // RegSrc1 must be 1 for LLB and LHB
-    assign RegSrc1 = Opcode[3] & Opcode[1];
+    // RegSrc must be 1 for LLB and LHB
+    assign RegSrc = Opcode[3] & Opcode[1];
 
-    // MemEnable is on for SW or LW
+    // MemEnable must be 1 for SW and LW
     assign MemEnable = Opcode[3] & ~Opcode[2] & ~Opcode[1];
 
     // MemWrite is only 1 for SW
@@ -39,11 +42,17 @@ module ControlUnit(Opcode, ALUSrc, MemtoReg, RegWrite, RegSrc1, MemRead, MemWrit
     // necessary operation for the given instruction
     assign ALUOp = Opcode;
 
-    // HLT Opcode = 1111
+    // HLT Opcode = 0x1111
     assign HLT = &Opcode[3:0];
 
-    // PCS Opcode = 1110
+    // PCS Opcode = 0x1110
     assign PCS = Opcode[3] & Opcode[2] & Opcode[1] & ~Opcode[0];
+    
+    // Z_en is enabled for all ALU instructions except PADDSB.
+    assign Z_en = ~opcode[3] & (~opcode[2] | ~opcode[1] | ~opcode[0]);
+  
+    // NV_en is enabled for just ADD and SUB instructions.
+    assign NV_en = opcode[3:1] == 3'h0;
 
 endmodule
 
--- Phase-1/cpu.v
+++ Phase-1/cpu.v
@@ -1,3 +1,5 @@
+`default_nettype none // Set the default as none to avoid errors
+
 /////////////////////////////////////////////
 // cpu.v                                  //
 //                                       //
@@ -28,30 +30,34 @@ module cpu (
   wire [3:0] Mem_offset;
   wire [7:0] LB_imm;
   wire [8:0] Brnch_imm;
-  wire [2:0] Condition;
+  wire [2:0] c_codes;
 
   // signals to determine which registers are read
   wire [3:0] SrcReg1;
   wire [3:0] SrcReg2;
   
   // data from registers
-  wire [15:0] rs_data;
-  wire [15:0] rt_data;
+  wire [15:0] SrcReg1_data;
+  wire [15:0] SrcReg2_data;
 
   // Control signals
   wire RegSrc;
   wire RegWrite;
   wire Branch;
   wire ALUSrc;
-  wire [2:0] ALUOp;
-  wire Z_en, V_en, N_en;
+  wire [3:0] ALUOp;
+  wire Z_en, NV_en;
+  wire Z_set, V_set, N_set;
   wire MemWrite;
-  wire MemEnable; // not sure if needed
+  wire MemEnable;
   wire MemToReg;
   wire HLT;
+  wire PCS;
 
   // ALU signals
-  wire [15:0] ALU_in2;
+  wire [15:0] imm; 
+  wire [15:0] ALU_In2_step;
+  wire [15:0] ALU_In2;
   wire [15:0] ALU_out;
 
   // Branch signals
@@ -63,7 +69,7 @@ module cpu (
   wire [15:0] MemData;
 
   // flag signals
-  wire Z, V, N;
+  wire ZF, VF, NF;
 
   /////////////////////////////////////////
   // Make reset active high for modules //
@@ -73,7 +79,8 @@ module cpu (
   ////////////////////////////////
   // Fetch Instruction from PC //
   //////////////////////////////
-  memory1c iPC(.data_out(pc_inst),
+  // Infer the instruction memory, it is always read enabled and never write enabled.
+  memory1c iINSTR_MEM (.data_out(pc_inst),
                .data_in(16'h0000),
                .addr(pc_addr),
                .enable(1'b1),
@@ -82,93 +89,128 @@ module cpu (
                .rst(rst)
                );
 
-  // TODO: Create PC Register
+  // Infer the PC Register.
+  PC_Register(.clk(clk), .rst(rst), .nxt_pc(nxt_pc), .curr_pc(pc_addr));
 
-  /////////////////////////////////////////////////////
-  // Decode instruction and get data from registers //
-  ///////////////////////////////////////////////////
-  // assign instruction bits
-  assign opcode = pc_inst[15:12];
-  assign reg_rd = pc_inst[11:8];
-  assign reg_rs = pc_inst[7:4];
-  assign reg_rt = pc_inst[3:0];
-
-  assign ALU_imm = pc_inst[3:0];
-  assign Mem_offset = pc_inst[3:0];
-  assign LB_imm = pc_inst[7:0];
-  assign Brnch_imm = pc_inst[8:0];
-  assign Condition = pc_inst[11:9];
-
-  // determine which register we are reading
-  assign SrcReg1 = (RegSrc) ? reg_rd : reg_rs;
-  assign SrcReg2 = (MemWrite) ? reg_rd : reg_rt;
-
-  // Read and Write data from registers
-  RegisterFile iRF(.clk(clk),
-                   .rst(rst),
-                   .SrcReg1(reg_rs),
-                   .SrcReg2(reg_rt),
-                   .DstReg(reg_rd),
-                   .WriteReg(RegWrite),
-                   .DstData(RegWriteData),
-                   .SrcData1(rs_data),
-                   .SrcData2(rt_data)
-                   );
-
-  // TODO: CONTROL UNIT
-
-  ///////////////////////////////////////////////////
-  // Execute Instruction based on control signals //
-  /////////////////////////////////////////////////
-  // determine 2nd ALU input
-  assign ALU_in2 = ALUSrc ? {12'h000, ALU_imm} : rt_data;
-
-  // sign extend memory offset immediate
-  SignExtender #(5) iMSE (.in(Mem_offset << 1), .out(Mem_ex_offset));
-
-  // TODO: ALU UNIT
-  // with ALU_out as rslt
-
-  // TODO: finish flag_register
-  flag_register iFR(.clk(clk),
-                    .rst(rst),
-                    .Z_en(Z_en), .Z_set(),
-                    .V_en(V_en), .V_set(),
-                    .N_en(N_en), .N_set(),
-                    .Z(Z),
-                    .V(V),
-                    .N(N)
-                    );
-
-  // determines what the next pc address is
-  PC_control iPCC (.C(Condition),
+  // Determines what the next pc address is based on branch taken/not.
+  PC_control iPCC (.C(c_codes),
                    .I(Branch_imm),
                    .F({Z, V, N}),
-                   .Rs(rs_data),
+                   .Rs(SrcReg1_data),
                    .Branch(Branch),
                    .BR(pc_inst[12]),
                    .PC_in(pc_addr),
                    .PC_out(nxt_pc)
                   );
 
+  /////////////////////////////////////////////////////
+  // Decode instruction and get data from registers //
+  ///////////////////////////////////////////////////
+  // Get the opcode, Rd, Rs, Rt register IDs.
+  assign opcode = pc_inst[15:12];
+  assign reg_rd = pc_inst[11:8];
+  assign reg_rs = pc_inst[7:4];
+  assign reg_rt = pc_inst[3:0];
+
+  // Get the immediate value for SLL/SRA/ROR/MEM/Branch instructions along with condition codes.
+  assign ALU_imm = pc_inst[3:0];
+  assign Mem_offset = pc_inst[3:0];
+  assign LB_imm = pc_inst[7:0];
+  assign Brnch_imm = pc_inst[8:0];
+  assign c_codes = pc_inst[11:9];
+
+  /* Determine which register we are reading. */
+  // Read from Rd for LLB/LHB instructions and Rs for remaining instructions.
+  assign SrcReg1 = (RegSrc) ? reg_rd : reg_rs;
+
+  // Read from Rd for store instructions or Rt for any other instructions.
+  assign SrcReg2 = (MemWrite) ? reg_rd : reg_rt;
+
+  // Instantiate the register file for the CPU.
+  RegisterFile iRF(.clk(clk),
+                   .rst(rst),
+                   .SrcReg1(SrcReg1),
+                   .SrcReg2(SrcReg2),
+                   .DstReg(reg_rd),
+                   .WriteReg(RegWrite),
+                   .DstData(RegWriteData),
+                   .SrcData1(SrcReg1_data),
+                   .SrcData2(SrcReg2_data)
+                   );
+
+  // Decodes the opcode and outputs the necessary control signals.
+  ControlUnit iCC(.Opcode(opcode), 
+                  .ALUSrc(ALUSrc), 
+                  .MemtoReg(MemToReg), 
+                  .RegWrite(RegWrite), 
+                  .RegSrc(RegSrc), 
+                  .MemEnable(MemEnable), 
+                  .MemWrite(MemWrite), 
+                  .Branch(Branch), 
+                  .HLT(HLT), 
+                  .PCS(PCS), 
+                  .ALUOp(ALUOp),
+                  .Z_en(Z_en),
+                  .NV_en(NV_en)
+                  );
+
+  ///////////////////////////////////////////////////
+  // Execute Instruction based on control signals //
+  /////////////////////////////////////////////////
+  // Grab the LLB/LHB immediate or the ALU immediate based on the instruction.
+  assign imm = (RegSrc) ? {8'h00, LB_imm} : {12'h000, ALU_imm};
+
+  // Determine the 2nd ALU input, either zero-extended immediate or SrcReg2 data (Rd for save word or Rt otherwise).
+  assign ALU_In2_step = (ALUSrc) ? imm : SrcReg2_data;
+
+  // Sign extend the immediate memory offset.
+  SignExtender #(4) iMSE (.in(Mem_offset << 1'b1), .out(Mem_ex_offset));
+
+  // Get the second ALU input based on whether it is LW/SW instruction or not.
+  assign ALU_In2 = (MemEnable) ? Mem_ex_offset : ALU_In2_step;
+
+  // Instantiate ALU.
+  ALU iALU (.ALU_In1(SrcData1),
+            .ALU_In2(ALU_In2),
+            .Opcode(ALUOp),
+            .ALU_Out(ALU_out),
+            .Z_set(Z_set),
+            .N_set(N_set),
+            .V_set(V_set)
+            );
+
+  // Instantiate the flag_register.
+  flag_register iFR (.clk(clk),
+                    .rst(rst),
+                    .Z_en(Z_en, .Z_set(Z_set),
+                    .V_en(NV_en), .V_set(N_set),
+                    .N_en(NV_en), .N_set(V_set),
+                    .Z(ZF),
+                    .V(VF),
+                    .N(NF)
+                    );
+
   ///////////////////////////////////////////////////////////////////////
   // Read or Write to Memory and write back to Register if applicable //
   /////////////////////////////////////////////////////////////////////
-  // TODO: Finish memory signals
-  // Read or Write to Memory
-  memory1c iMEM(.data_out(MemData),
-                .data_in(rt_data),
-                .addr(ALU_out),
-                .enable(MemEnable),
-                .wr(MemWrite),
-                .clk(clk),
-                .rst(rst)
-                );
+  // Instantiate the data memory. 
+  memory1c iDATA_MEM (.data_out(MemData),
+                      .data_in(SrcReg2_data),
+                      .addr(ALU_out),
+                      .enable(MemEnable),
+                      .wr(MemWrite),
+                      .clk(clk),
+                      .rst(rst)
+                      );
 
   ///////////////////////////////////////////////////////////
   // Determine what is being written back to the register //
   /////////////////////////////////////////////////////////
-  assign RegWriteData = MemToReg ? MemData : ALU_out;
-
+  // Grab the data from memory for LW for write back or if it's PCS, we send (PC+2) for write back,
+  // otherwise send the ALU output.
+  assign RegWriteData = (MemToReg) ? MemData : 
+                                      (PCS) ? nxt_pc : ALU_out;
 
 endmodule
+
+`default_nettype wire  // Reset default behavior at the end

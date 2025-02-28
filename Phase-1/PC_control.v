3fc893988ad204df3d271dd57fefa3f6e51b4a54
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-02-28 08:24:00 -0600

    Added PC_control unit.

--- Phase-1/PC_control.v
+++ Phase-1/PC_control.v
@@ -17,7 +17,7 @@ module PC_control(C, I, F, PC_in, PC_out);
   
   input wire [2:0] C;        // 3-bit condition code
   input wire [8:0] I;        // 9-bit signed offset right shifted by one
-  input wire [2:0] F;        // 3-bit flag register inputs for (Z, N, V)
+  input wire [2:0] F;        // 3-bit flag register inputs for (F[2] = Z, F[1] = V, F[0] = N)
   input wire [15:0] PC_in;   // 16-bit address of the current instruction
   output wire [15:0] PC_out; // 16-bit address of the new instruction
 

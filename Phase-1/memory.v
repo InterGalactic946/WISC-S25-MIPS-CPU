de12e988b9ed16be903efe6020afab8bc132cf00
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-02 00:07:45 -0600

    Added verification tasks.

--- Phase-1/memory.v
+++ Phase-1/memory.v
@@ -54,9 +54,9 @@ module memory1c (data_out, data_in, data, addr, enable, wr, clk, rst);
          //load loadfile_all.img
          if (!loaded) begin
             if (data)
-              $readmemh("/WISC-S25-MIPS-CPU/Phase-1/data.img", mem);
+              $readmemh("./data.img", mem);
             else
-              $readmemh("/WISC-S25-MIPS-CPU/Phase-1/instructions.img", mem);
+              $readmemh("./instructions.img", mem);
             loaded = 1;
          end
           

7c13b309641c568fe04f51e681aa63df122cc3db
kkraft7039 <146286325+kkraft7039@users.noreply.github.com>
2025-03-01 18:28:13 -0600

    Changed memory.v to take different files

--- Phase-1/memory.v
+++ Phase-1/memory.v
@@ -30,39 +30,48 @@
 
 module memory1c (data_out, data_in, addr, enable, wr, clk, rst);
 
-   parameter ADDR_WIDTH = 16;
-   output  [15:0] data_out;
-   input [15:0]   data_in;
-   input [ADDR_WIDTH-1 :0]   addr;
-   input          enable;
-   input          wr;
-   input          clk;
-   input          rst;
-   wire [15:0]    data_out;
+  parameter FILE = 0;
+  parameter ADDR_WIDTH = 16;
+  output  [15:0] data_out;
+  input [15:0]   data_in;
+  input [ADDR_WIDTH-1 :0]   addr;
+  input          enable;
+  input          wr;
+  input          clk;
+  input          rst;
+  wire [15:0]    data_out;
    
-   reg [15:0]      mem [0:2**ADDR_WIDTH-1];
-   reg            loaded;
+  reg [15:0]      mem [0:2**ADDR_WIDTH-1];
+  reg            loaded;
    
-   assign         data_out = (enable & (~wr))? {mem[addr[ADDR_WIDTH-1 :1]]}: 0; //Read
-   initial begin
-      loaded = 0;
-   end
+  assign         data_out = (enable & (~wr))? {mem[addr[ADDR_WIDTH-1 :1]]}: 0; //Read
+  initial begin
+    loaded = 0;
+  end
 
-   always @(posedge clk) begin
-      if (rst) begin
-         //load loadfile_all.img
-         if (!loaded) begin
+  always @(posedge clk) begin
+	  if (rst) begin
+      //load img file
+      if (!loaded) begin
+        generate
+          if (FILE == 0)
             $readmemh("loadfile_all.img", mem);
-            loaded = 1;
-         end
+          else if (FILE == 1)
+            $readmemh("instruction.img", mem);
+          else
+            $readmemh("data.img", mem);
+        endgenerate
+        
+        loaded = 1;
+      end
           
+    end
+    else begin
+      if (enable & wr) begin
+	      mem[addr[ADDR_WIDTH-1 :1]] = data_in[15:0];       // The actual write
       end
-      else begin
-         if (enable & wr) begin
-	        mem[addr[ADDR_WIDTH-1 :1]] = data_in[15:0];       // The actual write
-         end
-      end
-   end
+    end
+  end
 
 
 endmodule 

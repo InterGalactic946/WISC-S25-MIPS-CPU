f241f12d53f71292da04cf48149c28b1f674543f
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-02 07:01:23 -0600

    Added verification tasks.

--- Phase-1/cpu_tb.sv
+++ Phase-1/cpu_tb.sv
@@ -53,6 +53,7 @@ module cpu_tb();
       
       // Initialize the PC to a starting value (e.g., 0)
       $display("Initializing CPU Testbench...");
+      next_pc = 16'h0000;
 
       // Initialize all signals for the testbench.
       Initialize(.clk(clk), .rst_n(rst_n));

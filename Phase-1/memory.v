6345b09f0a516e545de52682be6f8d799e1c17c4
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-03 11:00:47 -0600

    Polished the files.

--- Phase-1/memory.v
+++ Phase-1/memory.v
@@ -54,7 +54,7 @@ module memory1c (data_out, data_in, data, addr, enable, wr, clk, rst);
          //load loadfile_all.img
          if (!loaded) begin
             if (data)
-              $readmemh($sformatf("%s/instructions.img", `get_dir_from_file(`__FILE__)), mem);
+              $readmemh($sformatf("%s/data.img", `get_dir_from_file(`__FILE__)), mem);
               
             else
               $readmemh($sformatf("%s/instructions.img", `get_dir_from_file(`__FILE__)), mem);

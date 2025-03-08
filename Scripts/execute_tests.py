5b89e424bc75264a31fc8d8a4dcf70a8d77f027d
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-07 23:07:41 -0600

    Modified script.

--- Scripts/execute_tests.py
+++ Scripts/execute_tests.py
@@ -277,8 +277,7 @@ def check_design_files():
         None
     """
     # Get absolute paths of all Verilog files (excluding testbench files).
-    verilog_files = [os.path.abspath(f) for f in DESIGNS_DIR]
-    print(verilog_files)
+    verilog_files = [os.path.abspath(f) for f in os.listdir(DESIGNS_DIR)]
     
     # List to store files that fail the check.
     failed_files = []

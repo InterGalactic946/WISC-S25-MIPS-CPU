759819d8c17ba3de25d91075726db1146b925bbe
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-19 22:56:36 -0500

    Updated a slew of modules

--- Phase-2/tests/Fetch_tb.sv
+++ Phase-2/tests/Fetch_tb.sv
@@ -286,7 +286,7 @@ module Fetch_tb();
         actual_taken = $random % 2;
       
       4, 5:  // 25% of the time, randomize actual_target
-      actual_target = (actual_taken) ? (16'h0000 + ($random % num_tests)) : 16'h0000;
+      actual_target = (actual_taken) ? (16'h0000 + ($random % num_tests) * 2) : 16'h0000;
 
       6:  // 12.5% of the time, randomize enable
         enable = $random % 2;
@@ -294,7 +294,7 @@ module Fetch_tb();
       default: begin  // 12.5% of the time, randomize everything
         is_branch = $random % 2;
         actual_taken = $random % 2;
-        actual_target = (actual_taken) ? (16'h0000 + ($random % num_tests)) : 16'h0000;
+        actual_target = (actual_taken) ? (16'h0000 + ($random % num_tests) * 2) : 16'h0000;
         enable = $random % 2;
       end
     endcase

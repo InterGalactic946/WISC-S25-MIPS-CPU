1f74ae135a71be7fda8449a6e531d61946d1eb43
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-19 22:34:01 -0500

    Updated a slew of modules

--- Phase-2/tests/Fetch_tb.sv
+++ Phase-2/tests/Fetch_tb.sv
@@ -239,7 +239,7 @@ module Fetch_tb();
       stalls = 0;
 
       // initialize num_tests.
-      num_tests = 16;
+      num_tests = 34;
 
       // Wait for the first clock cycle to assert reset
       @(posedge clk);

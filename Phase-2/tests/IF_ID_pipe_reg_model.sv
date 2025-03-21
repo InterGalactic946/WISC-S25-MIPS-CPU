ec8c8d648fb541a7f0e844dcd424cb82672abfb5
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-21 10:45:10 -0500

    Added model regs.

--- Phase-2/tests/IF_ID_pipe_reg_model.sv
+++ Phase-2/tests/IF_ID_pipe_reg_model.sv
@@ -4,21 +4,21 @@
 // This module represents a model IF/ID pipeline register for the CPU.   //
 ///////////////////////////////////////////////////////////////////////////
 module IF_ID_pipe_reg_model (
-    input wire clk,                     // System clock
-    input wire rst,                     // Active high synchronous reset
-    input wire stall,                   // Stall signal (prevents updates)
-    input wire flush,                   // Flush pipeline register (clears the instruction word)
-    input wire [15:0] PC_curr,          // Current PC from the fetch stage
-    input wire [15:0] PC_next,          // Next PC from the fetch stage
-    input wire [15:0] PC_inst,          // Current instruction word from the fetch stage
-    input wire [1:0] prediction,        // The 2-bit predicted value of the current branch instruction from the fetch stage
-    input wire [15:0] predicted_target, // The predicted target from the BTB.
+    input logic clk,                     // System clock
+    input logic rst,                     // Active high synchronous reset
+    input logic stall,                   // Stall signal (prevents updates)
+    input logic flush,                   // Flush pipeline register (clears the instruction word)
+    input logic [15:0] PC_curr,          // Current PC from the fetch stage
+    input logic [15:0] PC_next,          // Next PC from the fetch stage
+    input logic [15:0] PC_inst,          // Current instruction word from the fetch stage
+    input logic [1:0] prediction,        // The 2-bit predicted value of the current branch instruction from the fetch stage
+    input logic [15:0] predicted_target, // The predicted target from the BTB.
     
-    output wire [3:0] IF_ID_PC_curr,            // Pipelined lower 4-bits of current instruction address passed to the decode stage
-    output wire [15:0] IF_ID_PC_next,           // Pipelined next PC passed to the decode stage
-    output wire [15:0] IF_ID_PC_inst,           // Pipelined current instruction word passed to the decode stage
-    output wire [1:0] IF_ID_prediction,         // Pipelined 2-bit branch prediction signal passed to the decode stage
-    output wire [15:0] IF_ID_predicted_target   // Pipelined 2-bit branch prediction signal passed to the decode stage
+    output logic [3:0] IF_ID_PC_curr,            // Pipelined lower 4-bits of current instruction address passed to the decode stage
+    output logic [15:0] IF_ID_PC_next,           // Pipelined next PC passed to the decode stage
+    output logic [15:0] IF_ID_PC_inst,           // Pipelined current instruction word passed to the decode stage
+    output logic [1:0] IF_ID_prediction,         // Pipelined 2-bit branch prediction signal passed to the decode stage
+    output logic [15:0] IF_ID_predicted_target   // Pipelined 2-bit branch prediction signal passed to the decode stage
 );
 
   ///////////////////////////////////////////////

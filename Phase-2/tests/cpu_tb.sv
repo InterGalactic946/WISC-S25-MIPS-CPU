2484cee0d3072c48901435e43833f7341c51c82b
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-23 01:53:31 -0500

    Added model regs.

--- Phase-2/tests/cpu_tb.sv
+++ Phase-2/tests/cpu_tb.sv
@@ -94,24 +94,27 @@ module cpu_tb();
   always @(negedge clk) begin
       if (rst_n) begin
         // Dump the contents of memory whenever we write to the BTB or BHT.
-        if (iDUT.wen_BHT || iDUT.wen_BTB)
+        if (iDUT.wen_BHT || iDUT.wen_BTB) begin
           log_BTB_BHT_dump (
             .model_BHT(iMODEL.iFETCH.iDBP_model.BHT),
             .model_BTB(iMODEL.iFETCH.iDBP_model.BTB),
             .dut_BHT(iDUT.iFETCH.iDBP.iBHT.iMEM_BHT.mem),
             .dut_BTB(iDUT.iFETCH.iDBP.iBTB.iMEM_BTB.mem)
           );
+        end
 
         // Log data memory contents.
-        if (iDUT.EX_MEM_MemEnable)
+        if (iDUT.EX_MEM_MemEnable) begin
           log_data_dump(
               .model_data_mem(iMODEL.iDATA_MEM.data_memory),     
               .dut_data_mem(iDUT.iDATA_MEM.mem)          
           );
+        end
         
         // Log the regfile contents.
-        if (iDUT.MEM_WB_RegWrite)
+        if (iDUT.MEM_WB_RegWrite) begin
           log_regfile_dump(.regfile(iMODEL.iDECODE.iRF.regfile));
+        end
       end
   end
 

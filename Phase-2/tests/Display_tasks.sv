272cbf12bd67a9f58f97009d966da0490e6a6172
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-23 11:22:42 -0500

    Added model regs.

--- Phase-2/tests/Display_tasks.sv
+++ Phase-2/tests/Display_tasks.sv
@@ -122,10 +122,13 @@ package Display_tasks;
     // Determine the type of hazard and generate the appropriate message
     if (load_use_hazard) begin
         hazard_type = "load-to-use";
+        $display("load");
     end else if (br_hazard) begin
         hazard_type = "Branch (BR)";
+        $display("BR hazard");
     end else if (b_hazard) begin
         hazard_type = "Branch (B)";
+        $display("B hazard");
     end else begin
         hazard_type = "unknown"; // Fallback case
     end

a27af1d6918cd51ee0ee928111ed887ba3d98a16
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-24 14:36:07 -0500

    Modified files.

--- Phase-2/tests/cpu_tb.sv
+++ Phase-2/tests/cpu_tb.sv
@@ -83,9 +83,7 @@ module cpu_tb();
     Initialize(.clk(clk), .rst_n(rst_n));
 
     // Run the simulation for each instruction in the instruction memory until HLT reaches WB.
-    // TimeoutTask(.sig(hlt), .clk(clk), .clks2wait(1000000), .signal("HLT"));
-
-    repeat(25) @(posedge clk);
+    TimeoutTask(.sig(hlt), .clk(clk), .clks2wait(1000000), .signal("HLT"));
 
     // If we reached here, that means all test cases were successful
     $display("YAHOO!! All tests passed.");

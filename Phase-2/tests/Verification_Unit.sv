10b317247fd516984b39cdfc8ef3c07e126e4652
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-24 11:14:23 -0500

    Updated PC.

--- Phase-2/tests/Verification_Unit.sv
+++ Phase-2/tests/Verification_Unit.sv
@@ -16,7 +16,7 @@ import Monitor_tasks::*;
 
 module Verification_Unit (
     input logic clk, rst,                  // Clock and rst 
-    //input string fetch_msg,                // Fetch stage message
+    //input string fetch_msg,              // Fetch stage message
     input string if_id_msg,                // IF/ID Register message
     input string decode_msg,               // Decode stage messages
     input string instruction_full_msg,     // Full instruction message
@@ -57,9 +57,9 @@ always @(posedge clk) begin
         memory_id  <= -3;
         wb_id <= -4;
 
-        pc_idx <= 0;
-        if_id_idx <= 0;
-        id_ex_idx <= 0;
+        // pc_idx <= 0;
+        // if_id_idx <= 0;
+        // id_ex_idx <= 0;
     end else begin
         if (!(stall || flush)) begin
             fetch_id  <= fetch_id + 1;
@@ -69,11 +69,11 @@ always @(posedge clk) begin
             wb_id <= wb_id + 1;
         end
 
-        if (stall || flush) begin
-            pc_idx <= pc_idx + 1;
-            if_id_idx <= if_id_idx + 1;
-            id_ex_idx <= id_ex_idx + 1;
-        end
+        // if (stall || flush) begin
+        //     pc_idx <= pc_idx + 1;
+        //     if_id_idx <= if_id_idx + 1;
+        //     id_ex_idx <= id_ex_idx + 1;
+        // end
     end
 end
 
@@ -86,8 +86,8 @@ always @(posedge clk) begin
         // end
         // Decode Stage (IF/ID pipeline register & decode)
         if (decode_id >= 0) begin
-            pipeline_msgs[decode_id].if_id_hz_message[if_id_idx] <= if_id_hz_message;
-            pipeline_msgs[decode_id].flush_message[if_id_idx] <= flush_message;
+            // pipeline_msgs[decode_id].if_id_hz_message[if_id_idx] <= if_id_hz_message;
+           // pipeline_msgs[decode_id].flush_message[if_id_idx] <= flush_message;
             pipeline_msgs[decode_id].decode_msg[0] <= decode_msg;
             pipeline_msgs[decode_id].decode_msg[1] <= instruction_full_msg;
             pipeline_msgs[decode_id].if_id_msg   <= if_id_msg;
@@ -97,8 +97,8 @@ always @(posedge clk) begin
 
         // Execute Stage (ID/EX pipeline register & execute)
         if (execute_id >= 0) begin
-            pipeline_msgs[execute_id].id_ex_hz_message[id_ex_idx] <= if_id_hz_message;
-            pipeline_msgs[execute_id].flush_message[id_ex_idx] <= flush_message;
+            // pipeline_msgs[execute_id].id_ex_hz_message[id_ex_idx] <= if_id_hz_message;
+            // pipeline_msgs[execute_id].flush_message[id_ex_idx] <= flush_message;
             pipeline_msgs[execute_id].id_ex_msg   <= id_ex_message;
             pipeline_msgs[execute_id].execute_msg   <= execute_msg;
             pipeline_msgs[execute_id].id_ex_cycle <= $time / 10;
@@ -128,27 +128,27 @@ end
 always @(posedge clk) begin
     if (!rst && wb_id >= 0) begin
         // Print messages for instruction reaching WB stage
-        $display("=======================================================");
-        $display("| Instruction: %s | Completed At Cycle: %0t |", pipeline_msgs[wb_id].decode_msg[1], $time/10);
-        $display("=======================================================");
+        $display("============================================================");
+        $display("        | Instruction: %s | Completed At Cycle: %0t |", pipeline_msgs[wb_id].decode_msg[1], $time/10);
+        $display("============================================================");
         // $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].fetch_msg, pipeline_msgs[wb_id].fetch_cycle);
         
-        if (stall)
-            print_stall_messages(.inst_id(wb_id), .size(if_id_idx), .msg_type("if_id"), .cycle(pipeline_msgs[wb_id].if_id_cycle));
-        else if (flush)
-            print_stall_messages(.inst_id(wb_id), .size(if_id_idx), .msg_type("flush"), .cycle(pipeline_msgs[wb_id].if_id_cycle));
-        else begin
-            $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].if_id_msg, pipeline_msgs[wb_id].if_id_cycle);
-            $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].decode_msg[0], pipeline_msgs[wb_id].decode_cycle);
-        end
+        // if (stall)
+        //     print_stall_messages(.inst_id(wb_id), .size(if_id_idx), .msg_type("if_id"), .cycle(pipeline_msgs[wb_id].if_id_cycle));
+        // else if (flush)
+        //     print_stall_messages(.inst_id(wb_id), .size(if_id_idx), .msg_type("flush"), .cycle(pipeline_msgs[wb_id].if_id_cycle));
+        // else begin
+        $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].if_id_msg, pipeline_msgs[wb_id].if_id_cycle);
+        $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].decode_msg[0], pipeline_msgs[wb_id].decode_cycle);
+        // end
 
-        if (stall)
-            print_stall_messages(.inst_id(wb_id), .size(id_ex_idx), .msg_type("id_ex"), .cycle(pipeline_msgs[wb_id].id_ex_cycle));
-        else if (flush)
-            print_stall_messages(.inst_id(wb_id), .size(id_ex_idx), .msg_type("flush"), .cycle(pipeline_msgs[wb_id].id_ex_cycle));
-        else begin
-            $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].id_ex_msg, pipeline_msgs[wb_id].id_ex_cycle);
-        end
+        // if (stall)
+        //     print_stall_messages(.inst_id(wb_id), .size(id_ex_idx), .msg_type("id_ex"), .cycle(pipeline_msgs[wb_id].id_ex_cycle));
+        // else if (flush)
+        //     print_stall_messages(.inst_id(wb_id), .size(id_ex_idx), .msg_type("flush"), .cycle(pipeline_msgs[wb_id].id_ex_cycle));
+        // else begin
+        $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].id_ex_msg, pipeline_msgs[wb_id].id_ex_cycle);
+        // end
 
         $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].execute_msg, pipeline_msgs[wb_id].execute_cycle);
         $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].ex_mem_msg, pipeline_msgs[wb_id].ex_mem_cycle);
@@ -160,18 +160,18 @@ always @(posedge clk) begin
 end
 
 
-    // Task: Prints the stall and flush messages.
-    task automatic print_stall_messages(input integer inst_id, integer size, input string msg_type, integer cycle);
-        integer i;
-        for (i = 0; i < size; i++) begin
-            if (msg_type == "pc" && pipeline_msgs[inst_id].pc_message[i] != "")
-                $display("|%s @ Cycle: %0t", pipeline_msgs[inst_id].pc_message[i], cycle);
-            else if (msg_type == "if_id" && pipeline_msgs[inst_id].if_id_hz_message[i] != "")
-                $display("|%s @ Cycle: %0t", pipeline_msgs[inst_id].if_id_hz_message[i], cycle);
-            else if (msg_type == "id_ex" && pipeline_msgs[inst_id].id_ex_hz_message[i] != "")
-                $display("|%s @ Cycle: %0t", pipeline_msgs[inst_id].id_ex_hz_message[i], cycle);
-            else if (msg_type == "flush" && pipeline_msgs[inst_id].flush_message[i] != "")
-                $display("|%s @ Cycle: %0t", pipeline_msgs[inst_id].flush_message[i], cycle);
-        end
-    endtask
+    // // Task: Prints the stall and flush messages.
+    // task automatic print_stall_messages(input integer inst_id, integer size, input string msg_type, integer cycle);
+    //     integer i;
+    //     for (i = 0; i < size; i++) begin
+    //         if (msg_type == "pc" && pipeline_msgs[inst_id].pc_message[i] != "")
+    //             $display("|%s @ Cycle: %0t", pipeline_msgs[inst_id].pc_message[i], cycle);
+    //         else if (msg_type == "if_id" && pipeline_msgs[inst_id].if_id_hz_message[i] != "")
+    //             $display("|%s @ Cycle: %0t", pipeline_msgs[inst_id].if_id_hz_message[i], cycle);
+    //         else if (msg_type == "id_ex" && pipeline_msgs[inst_id].id_ex_hz_message[i] != "")
+    //             $display("|%s @ Cycle: %0t", pipeline_msgs[inst_id].id_ex_hz_message[i], cycle);
+    //         else if (msg_type == "flush" && pipeline_msgs[inst_id].flush_message[i] != "")
+    //             $display("|%s @ Cycle: %0t", pipeline_msgs[inst_id].flush_message[i], cycle);
+    //     end
+    // endtask
 endmodule

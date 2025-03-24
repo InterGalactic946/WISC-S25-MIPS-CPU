186fc8b6e91753467283a17658faa9e6aa85432a
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-24 13:51:43 -0500

    Modified files.

--- Phase-2/tests/Verification_Unit.sv
+++ Phase-2/tests/Verification_Unit.sv
@@ -88,10 +88,10 @@ module Verification_Unit (
                 pipeline_msgs[memory_id].memory_cycle <= $time / 10;
             end
             if (valid_wb) begin
-                pipeline_msgs[wb_id].mem_wb_msg <= mem_wb_msg;
-                pipeline_msgs[wb_id].mem_wb_cycle <= $time / 10;
-                pipeline_msgs[wb_id].wb_msg <= wb_msg;
-                pipeline_msgs[wb_id].wb_cycle <= $time / 10;
+                pipeline_msgs[wb_id].mem_wb_msg = mem_wb_msg;
+                pipeline_msgs[wb_id].mem_wb_cycle = $time / 10;
+                pipeline_msgs[wb_id].wb_msg = wb_msg;
+                pipeline_msgs[wb_id].wb_cycle = $time / 10;
             end
         end
     end

311d98a2f80865e104d810dc4109854e1957c174
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-25 08:11:07 -0500

    Modified files.

--- Phase-2/tests/Verification_Unit.sv
+++ Phase-2/tests/Verification_Unit.sv
@@ -146,7 +146,12 @@ end
     always @(posedge clk) begin
         if (!rst && valid_wb) begin
             $display("==========================================================");
-            $display("| Instruction: %s | Completed At Cycle: %0t |", pipeline_msgs[wb_id].decode_msg[1], $time / 10);
+            if (!stall && !flush)
+                $display("| Instruction: %s | Completed At Cycle: %0t |", pipeline_msgs[wb_id].decode_msg[1], $time / 10);
+            else if (stall)
+                $display("| Instruction: NOP | Completed At Cycle: %0t |", $time / 10);
+            else if (flush)
+                $display("| Instruction: %s (NOP) | Completed At Cycle: %0t |", pipeline_msgs[wb_id].decode_msg[1], $time / 10);
             $display("==========================================================");
             $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].if_id_msg, pipeline_msgs[wb_id].if_id_cycle);
             $display("|%s @ Cycle: %0t", pipeline_msgs[wb_id].decode_msg[0], pipeline_msgs[wb_id].decode_cycle);

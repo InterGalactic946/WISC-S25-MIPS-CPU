651d917a8c10f6e219b0b57b297351bd0b669f88
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-25 16:58:50 -0500

    Modified files.

--- Phase-2/tests/cpu_tb.sv
+++ Phase-2/tests/cpu_tb.sv
@@ -228,7 +228,7 @@ module cpu_tb();
     end
 
     // Print the message for each instruction.
-    always @(posedge clk) begin
+    always @(negedge clk) begin
         if (!rst && valid_wb) begin
             $display("==========================================================");
             $display("| Instruction: %s | Completed At Cycle: %0t |", pipeline_msgs[wb_id].decode_msg[1], $time / 10);

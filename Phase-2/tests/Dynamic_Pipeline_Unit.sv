583bfa5c6f9141570d370d75f6ff4c3f7ad0652e
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-27 11:50:59 -0500

    Modified files.

--- Phase-2/tests/Dynamic_Pipeline_Unit.sv
+++ Phase-2/tests/Dynamic_Pipeline_Unit.sv
@@ -105,7 +105,7 @@ module Dynamic_Pipeline_Unit (
                 end
 
                 // Handle stage transition for the first instruction to FETCH
-                pipeline[0].stage = FETCH;
+                pipeline[0] = '{FETCH, '{default: ""}, '{default: ""}, "", "", "", "", 0};
             end
         end
     end

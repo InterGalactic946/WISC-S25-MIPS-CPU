29c7d998c5fea646e673e1c9eb873f20c2903a2f
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-31 17:58:29 -0500

    Fixed files and cleaned up code.

--- Phase-2/tests/Verification_Unit.sv
+++ Phase-2/tests/Verification_Unit.sv
@@ -72,8 +72,12 @@
             valid_execute <= valid_decode;
             valid_memory <= valid_execute;
             valid_wb <= valid_memory;
-        end else begin
-            valid_fetch <= 1;
+        end else if (stall) begin
+            valid_fetch <= 0;
+            valid_decode <= 0;
+            valid_execute <= valid_decode;
+            valid_memory <= valid_execute;
+            valid_wb <= valid_memory;
         end
     end
 

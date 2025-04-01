4fb0230212c6baca0ea0b82f6176b400172945b3
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-03-31 20:14:32 -0500

    Fixed files and cleaned up code.

--- Phase-2/tests/Verification_Unit.sv
+++ Phase-2/tests/Verification_Unit.sv
@@ -177,7 +177,7 @@
                         $display("%s", pipeline_msgs[wb_id].decode_msgs[j]);
 
             // Don't print execute, memory, and wb messages if the instruction is HLT.
-            if (instr_full_msg !== "HLT") begin
+            if (pipeline_msgs[wb_id].instr_full_msg !== "HLT") begin
                 $display("%s", pipeline_msgs[wb_id].execute_msg);
                 $display("%s", pipeline_msgs[wb_id].memory_msg);
                 $display("%s", pipeline_msgs[wb_id].wb_msg);

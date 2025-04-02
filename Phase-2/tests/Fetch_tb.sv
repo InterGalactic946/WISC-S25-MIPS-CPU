2d538ca1ca227b9bb929ab2ddd6061f7c0228e99
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-04-01 22:28:59 -0500

    Added some more test files.

--- Phase-2/tests/Fetch_tb.sv
+++ Phase-2/tests/Fetch_tb.sv
@@ -149,7 +149,7 @@ module Fetch_tb();
       stalls = 0;
 
       // initialize num_tests.
-      num_tests = 32;
+      num_tests = 12; // Number of tests to run.
 
       // Wait for the first clock cycle to assert reset
       @(posedge clk);

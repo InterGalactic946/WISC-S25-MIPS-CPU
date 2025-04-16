2d825b9aad0147fbe4dbc95fc749de0f442894ec
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-04-16 13:49:25 -0500

    Added a 6:64 decoder.

--- Phase-3/designs/memory_system.v
+++ Phase-3/designs/memory_system.v
@@ -10,6 +10,8 @@
 module memory_system (
     input  wire         clk,
     input  wire         rst,                    // Active-high reset
+    input  wire         enable,                 // Enable for the memory
+    input  wire         on_chip_wr,             // Write enable from on chip
     input  wire [15:0]  on_chip_memory_address, // Address used to index into the cache (can also be the miss address)
     input  wire [15:0]  on_chip_memory_data,    // Memory data coming from the on chip processor core
 
@@ -26,6 +28,8 @@ module memory_system (
     /////////////////////////////////////////////////
     // Declare any internal signals as type wire  //
     ///////////////////////////////////////////////
+    wire wr_data_enable;       // Condition we write to the cache data array.
+    wire wr_tag_enable;        // Condition we write to the cache data array.
     wire [5:0] tag;            // Extract upper 6 bits of address as tag.
     wire [63:0] set_enable;    // One hot set enable for the 64 sets in the cache.
     wire [15:0] data_in;       // Data input to the cache.
@@ -37,7 +41,7 @@ module memory_system (
     ///////////////////////////////////////////////
 
     // Instantiate a 3:8 decoder to get which word of the 8 words to write to.
-    Decoder_3_8 iWORD_DECODER (.RegId(on_chip_memory_address[3:1]), .Wordline(word_enable));
+    Decoder_3_8 iWORD_DECODER (.RegId(on_chip_memory_address[3:1]), en(1'b1), .Wordline(word_enable));
 
     // Instantiate a 6:64 read decoder to get which set of the 64 sets to enable.
     Decoder_6_64 iSET_DECODER (.RegId(on_chip_memory_address[9:4]), .Wordline(set_enable));
@@ -52,14 +56,18 @@ module memory_system (
         .SetEnable(set_enable),
         
         .DataIn(data_in),
-        .WriteDataArray(write_data_array),
+        .WriteDataArray(wr_data_enable),
         .WordEnable(word_enable),
         
-        .WriteTagArray(write_tag_array),
+        .WriteTagArray(wr_tag_enable),
         
         .DataOut(data_out),
         .hit(hit)
     );
+    
+    // We write to the cache either when we have a hit and we are writing from on-chip, or we 
+    // have a miss and writing from off chip.
+    assign wr_data_enable = (hit) ? on_chip_wr : write_data_array;
 
     // Grab the tag as the upper 6 bits of the requested address.
     assign tag = on_chip_memory_address[15:10];

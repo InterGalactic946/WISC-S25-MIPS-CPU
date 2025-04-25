a803c34624fe94d2a07b7e3a8c1f24237240a224
Srivibhav-J <sjonnalagad2@wisc.edu>
2025-04-24 22:54:42 -0500

    Updated files.

--- Phase-3/tests/project_phase3_tb.v
+++ Phase-3/tests/project_phase3_tb.v
@@ -49,16 +49,14 @@ module project_phase3_tb();
    /* Setup */
    initial begin
       $display("Hello world...simulation starting");
-      $display("See verilogsim.plog and verilogsim.ptrace for output");
+      $display("YAHOO!! All tests passed. See verilogsim.log and verilogsim.trace for output");
       inst_count = 0;
       DCacheHit_count = 0;
       ICacheHit_count = 0;
       DCacheReq_count = 0;
       ICacheReq_count = 0;
-
-      trace_file = $fopen("verilogsim.ptrace");
-      sim_log_file = $fopen("verilogsim.plog");
-      
+      trace_file = $fopen("./outputs/verilogsim.trace");
+      sim_log_file = $fopen("./outputs/verilogsim.log");      
    end
 
 
@@ -172,43 +170,43 @@ module project_phase3_tb();
    // Is processor halted (1 bit signal)
    
 
-   assign Inst = DUT.p0.instr;
+   assign Inst = DUT.iPROC.PC_inst;
    //Instruction fetched in the current cycle
    
-   assign RegWrite = DUT.p0.regWrite;
+   assign RegWrite = DUT.iPROC.MEM_WB_RegWrite;
    // Is register file being written to in this cycle, one bit signal (1 means yes, 0 means no)
   
-   assign WriteRegister = DUT.p0.DstwithJmout;
+   assign WriteRegister = DUT.iPROC.MEM_WB_reg_rd;
    // If above is true, this should hold the name of the register being written to. (4 bit signal)
    
-   assign WriteData = DUT.p0.wData;
+   assign WriteData = DUT.iPROC.RegWriteData;
    // If above is true, this should hold the Data being written to the register. (16 bits)
    
-   assign MemRead =  (DUT.p0.memRxout & ~DUT.p0.notdonem);
+   assign MemRead =  (DUT.iPROC.EX_MEM_MemEnable & ~DUT.iPROC.EX_MEM_MemWrite);
    // Is memory being read from, in this cycle. one bit signal (1 means yes, 0 means no)
    
-   assign MemWrite = (DUT.p0.memWxout & ~DUT.p0.notdonem);
+   assign MemWrite = (DUT.iPROC.EX_MEM_MemEnable & DUT.iPROC.EX_MEM_MemWrite);
    // Is memory being written to, in this cycle (1 bit signal)
    
-   assign MemAddress = DUT.p0.data1out;
+   assign MemAddress = DUT.iPROC.EX_MEM_ALU_out;
    // If there's a memory access this cycle, this should hold the address to access memory with (for both reads and writes to memory, 16 bits)
    
-   assign MemDataIn = DUT.p0.data2out;
+   assign MemDataIn = DUT.iPROC.MemWriteData;
    // If there's a memory write in this cycle, this is the Data being written to memory (16 bits)
    
-   assign MemDataOut = DUT.p0.readData;
+   assign MemDataOut = DUT.iPROC.MemData;
    // If there's a memory read in this cycle, this is the data being read out of memory (16 bits)
 
-   assign ICacheReq = DUT.p0.icr;
+   assign ICacheReq = 1'b1;
    // Signal indicating a valid instruction read request to cache
    
-   assign ICacheHit = DUT.p0.ich;
+   assign ICacheHit = DUT.iPROC.ICACHE_hit;
    // Signal indicating a valid instruction cache hit
 
-   assign DCacheReq = DUT.p0.dcr;
+   assign DCacheReq = DUT.iPROC.EX_MEM_MemEnable;
    // Signal indicating a valid instruction data read or write request to cache
    
-   assign DCacheHit = DUT.p0.dch;
+   assign DCacheHit = DUT.iPROC.DCACHE_hit;
    // Signal indicating a valid data cache hit
 
 
